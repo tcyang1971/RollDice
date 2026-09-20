80f4ce64ee25e2f0bfc1d1e8b229cde6bd125c1d
tcyang1971 <tcyang@gm.pu.edu.tw>
2026-09-20 09:59:39 +0800

    V1：基本介面

--- app/src/main/java/tw/edu/pu/csim/tcyang/rolldice/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/rolldice/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tw.edu.pu.csim.tcyang.rolldice.ui.theme.RollDiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RollDiceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Dice(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Dice(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RollDiceTheme {
-        Greeting("Android")
+        Text(
+            text = "請點擊圖片隨機丟骰子\n作者：楊子青",
+            modifier = modifier
+        )
+
+        // 增加 20.dp 的垂直間距
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.dice0),
+            contentDescription = "Dice"
+        )
     }
 }
